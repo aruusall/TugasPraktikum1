9a746400c62cacdeeebf8f0f962195484b5e14b2
aruusall <56145908+aruusall@users.noreply.github.com>
2020-12-08 13:15:37 +0700

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,21 @@
+// aritmatika
+console.log("Aritmatika")
+let a = 2, b = 5, c = "10"
+console.log("a + b = " + (a + b))
+console.log("a + c = " + (a + c))
+console.log("a - b = " + (a - b))
+console.log("a * b = " + (a * b))
+console.log("b / a = " + (b / a))
+console.log("b % a = " + (b % a))
+console.log("b ** a = " + (b ** a))
+//perbandingan
+console.log("Perbandingan")
+let d = 5, e = 10, f = "5"
+console.log("a == c -> "+(a == c))
+console.log("a === c -> "+(a === c))
+console.log("a != c -> "+(a != c))
+console.log("a !== c -> "+(a !== c))
+console.log("a > c -> "+(a > c))
+console.log("a >= c -> "+(a >= c))
+console.log("a < c -> "+(a < c))
+console.log("a <= c -> "+(a <= c))
